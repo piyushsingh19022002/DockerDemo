9b395e60d8fbc8d37860874432c3cbda3771c3b1
piyushsingh19022002 <piyushsenger205@gmail.com>
2026-05-01 17:44:14 +0530

    app file updated

--- app.py
+++ app.py
@@ -7,5 +7,5 @@ def home():
     return "<h1>Devops Succesful</h1>" \
     "<p> this include python+docker+git+AWS</p>"
 
-if __main__==__name__ :
+if __main__=="__name__" :
     app.run(host='0.0.0.0',port=5000)
